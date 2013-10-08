872ebe4af1deba06b5274f26fef5f6f2515d35b8
Hack Reactor Students <georgebonnr@gmail.com>
2013-10-07 17:02:52 -0700

    Adding basic functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,33 +5,76 @@ var getData =  function() {
     // always use this url
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
-    contentType: 'application/json',
     success: function (data) {
       render(data.results);
     },
     error: function (data) {
-      console.error('chatterbox: Failed to send message');
+      console.error('chatterbox: Failed to retrieve messages');
     }
   });
 };
 
 var render = function (data) {
-  for (var i = 0; i < data.length; i++) {
+  data = _(data).filter(function (datum){
+    return (datum.roomname === window.roomname);
+  });
+  $('.chatList').html('');
+  for (var i = Math.max(0,data.length - window.pageSize); i < data.length; i++) {
     var datum = data[i];
-    var newMsg = "<li class='msg'>"+datum.text+"</li>";
+    var username = $("<span class='username'></span>");
+    username.text(datum.username + ": ");
+    var msgtext = $("<span class='msgtext'></span>");
+    msgtext.text(datum.text);
+    var newMsg = $("<li class='msg'></li>");
+    newMsg.append(username);
+    newMsg.append(msgtext);
     $('.chatList').append(newMsg);
   }
+  setTimeout(getData,100);
+};
+
+var sendData = function(data) {
+  data = JSON.stringify(data);
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    contentType: 'application/json',
+    data: data,
+    success: function (data) {
+      console.log('chatterbox: Message sent ' + data);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message ' + data);
+    }
+  });
 };
 
 $(document).on("ready", function() {
 
-  getData();
+  // Global variables.
+  window.pageSize = 15;
+  window.roomname = "lobby";
 
-  // Input.
+  // Assign all parameters in the URL to global variables.
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    vars[i] = vars[i].split("=");
+    window[vars[i][0]] = vars[i][1];
+  }
+
+
+  // JQuery bindings.
   $('input').bind("enterText",function(e){
-    var newMsg = "<li class='msg'>"+$(this).val()+"</li>";
-    $('.chatList').append(newMsg);
-    $(this).val('');
+    if ($(this).val()) {
+      var message = {
+        username: window.username,
+        text: $(this).val(),
+        roomname: window.roomname
+      };
+      sendData(message);
+      $(this).val('');
+    }
   });
 
   $('input').keyup(function(e){
@@ -40,4 +83,8 @@ $(document).on("ready", function() {
           $(this).trigger("enterText");
       }
   });
+
+
+  // Page initialization.
+  getData();
 });
